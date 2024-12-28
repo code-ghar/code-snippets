72d4ac0b0918fdebd813ca01d8d86e579a19f2bb
ritikrai999 <ritikrai999@gmail.com>
2024-12-29 05:25:59 +0530

    string algori

--- ritik/algorithms/stringAlgos.cpp
+++ ritik/algorithms/stringAlgos.cpp
@@ -139,6 +139,15 @@ vector<int> manacher_odd(string s) {
     return vector<int>(begin(p) + 1, end(p) - 1);
 }
 
+vector<int> manacher(string s) {
+    string t;
+    for(auto c: s) {
+        t += string("#") + c;
+    }
+    auto res = manacher_odd(t + "#");
+    return vector<int>(begin(res) + 1, end(res) - 1);
+}
+
 void solve()
 {
 
